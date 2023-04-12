b82649e2c5c1aee33f9c4644bac9f1d9829e89fa
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-04-12 09:30:29 +0200

    Kolonner vender rigtigt

--- app.js
+++ app.js
@@ -2,3 +2,14 @@
 
 console.log("Js kører");
 
+window.addEventListener("load", start);
+
+function start() {
+    console.log("JS kører");
+    const bar = document.querySelectorAll(".bar");
+    const firstBar = bar[0];
+
+    let h = 10;
+
+    firstBar.style.height = `${h}px`;
+}
