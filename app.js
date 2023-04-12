ea520e2920cc9b368257b76e14dbfdd86eba9575
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-04-12 09:45:14 +0200

    Kan sætte højden på et element fra array

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ console.log("Js kører");
 
 window.addEventListener("load", start);
 
+const data = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 5, 17, 30, 8, 21, 2, 14, 27, 9,
+  22];
+
 function start() {
     console.log("JS kører");
     const bar = document.querySelectorAll(".bar");
     const firstBar = bar[0];
 
-    let h = 10;
+    let h = data[0]/ 32* 100;
 
     firstBar.style.height = `${h}px`;
 }
