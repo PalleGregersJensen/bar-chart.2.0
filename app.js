4c476738d05e14d11f7d62abc199936ab161e112
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-04-12 09:56:41 +0200

    Alle bars virker og kan vise barchart

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-"use strict"
+"use strict";
 
 console.log("Js kører");
 
 window.addEventListener("load", start);
 
 const data = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-  22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 5, 17, 30, 8, 21, 2, 14, 27, 9,
-  22];
+  10, 1, 20, 30, 14, 25, 16, 7, 18, 9, 14, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23, 4, 15, 26, 7, 28, 29, 3, 1, 32, 5, 17, 30, 8, 21, 2, 14, 27, 9,
+  22,
+];
 
 function start() {
-    console.log("JS kører");
-    const bar = document.querySelectorAll(".bar");
-    const firstBar = bar[0];
+  console.log("JS kører");
+  displayAllBars();
+}
 
-    let h = data[0]/ 32* 100;
+function displayAllBars() {
+  const bar = document.querySelectorAll(".bar");
 
+  for (let i = 0; i < 40; i++) {
+    const firstBar = bar[i];
+    let h = (data[i] / 32) * 100;
     firstBar.style.height = `${h}px`;
+  }
 }
